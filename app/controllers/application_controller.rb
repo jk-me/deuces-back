daacd75b71d84470384f2346492fbefa588e5662
Jenny Kam <jkam17@gmail.com>
2020-02-21 14:03:11 -0500

    add root route for api, fallback route used for single repo app does not include root

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
 
   def fallback_index_html
-    render :file => 'public/index.html'
+    render :file => 'app/views/index.html.erb'
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   resources :games, only: [:index, :show, :create, :update, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'application#fallback_index_html'
   get '*path', to: "application#fallback_index_html", constraints: ->(request) do
     !request.xhr? && request.format.html?
   end
-  
+
 end
